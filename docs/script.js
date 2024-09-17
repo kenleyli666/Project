cff98b86f5f452bcad8f434a31fc7ebc6ea17c4e
kenleyli666 <107999510+kenleyli666@users.noreply.github.com>
2024-09-18 02:59:54 +0800

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -76,7 +76,7 @@ if (window.innerWidth <= 1024) {
     else{
         headItem2.style.display = "inline-flex";
         headItem4.style.display = "none";
-        menutoggle.style.right = "25px";
+        menutoggle.style.right = "20px";
     }
 }
 })
